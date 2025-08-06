01c0bd96cf76d9724611f0eb6469f6448bb6578d
u4ik <insighteuphoric@gmail.com>
2025-08-06 02:38:38 -0400

    fixed about

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
-   base: '/', 
-})
+  plugins: [react(), tailwindcss()],
+  base: "/",
+});
